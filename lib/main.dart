505c9ba92fb93d81bc7a8940a54cecf6aaeadffb
Simran <simran.simrangupta@gmail.com>
2023-01-07 14:15:54 +0530

    question

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,47 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
   void answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+
     print('Button pressed!');
+    print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
+    var question = [
+      'what is your fav human?',
+      'what is your fav fruit?',
+      'hu',
+      'huhu',
+      'ji',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('My first App')),
         body: Column(
           children: [
-            Text('The question!'),
+            Question(
+              question[_questionIndex],
+            ),
             ElevatedButton(
               child: Text('Answer 1'),
               onPressed: answerQuestion,
